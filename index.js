f82663ad74131104baf820ef69fbe07805e5dd46
Birgit Bartenschlager <birgit.bartenschlager@web.de>
2023-07-10 13:44:50 +0200

    random dice values

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
-/*
-Challenge: 
-1. Take the hard-coded HTML for the Wizard card, bring it 
-   into index.js and then inject it back into its div with 
-   JavaScript.
-2. Do the same for Orc card. 
-*/
+const hero = {
+  elementId: "hero",
+  name: "Wizard",
+  avatar: "images/wizard.png",
+  health: 60,
+  diceRoll: [3, 1, 4],
+  diceCount: 3,
+};
+
+const monster = {
+  elementId: "monster",
+  name: "Orc",
+  avatar: "images/orc.png",
+  health: 10,
+  diceRoll: [4],
+  diceCount: 1,
+};
+
+function getDiceRollArray(diceCount) {
+  return new Array(diceCount)
+    .fill(0)
+    .map(
+      () => `<div class="dice">
+    ${Math.floor(Math.random() * 6) + 1}
+    </div>`
+    )
+    .join("");
+}
+
+function renderCharacter({ elementId, name, avatar, health, diceCount }) {
+  document.getElementById(elementId).innerHTML = `
+   <div class="character-card">
+      <h4 class="name">${name}</h4>
+      <img class="avatar" src="${avatar}" />
+      <p class="health">health: <b> ${health} </b></p>
+      <div class="dice-container">
+        ${getDiceRollArray(diceCount)}
+      </div>
+   </div>
+`;
+}
+
+renderCharacter(hero);
+renderCharacter(monster);
