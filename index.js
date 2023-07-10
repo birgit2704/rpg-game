0cf8cf2396a18266309538321bca229b1ead6807
Birgit Bartenschlager <birgit.bartenschlager@web.de>
2023-07-10 14:16:03 +0200

    refactor code

--- index.js
+++ index.js
@@ -1,44 +1,30 @@
-const hero = {
-  elementId: "hero",
-  name: "Wizard",
-  avatar: "images/wizard.png",
-  health: 60,
-  diceRoll: [3, 1, 4],
-  diceCount: 3,
-};
-
-const monster = {
-  elementId: "monster",
-  name: "Orc",
-  avatar: "images/orc.png",
-  health: 10,
-  diceRoll: [4],
-  diceCount: 1,
-};
+import characters from "./data.js";
 
 function getDiceRollArray(diceCount) {
   return new Array(diceCount)
     .fill(0)
-    .map(
-      () => `<div class="dice">
-    ${Math.floor(Math.random() * 6) + 1}
-    </div>`
-    )
+    .map(() => Math.floor(Math.random() * 6) + 1);
+}
+
+function getDiceHtml(diceCount) {
+  return getDiceRollArray(diceCount)
+    .map((dice) => `<div class="dice">${dice}</div>`)
     .join("");
 }
 
 function renderCharacter({ elementId, name, avatar, health, diceCount }) {
+  const diceHtml = getDiceHtml(diceCount);
   document.getElementById(elementId).innerHTML = `
    <div class="character-card">
       <h4 class="name">${name}</h4>
       <img class="avatar" src="${avatar}" />
       <p class="health">health: <b> ${health} </b></p>
       <div class="dice-container">
-        ${getDiceRollArray(diceCount)}
+        ${diceHtml}
       </div>
    </div>
 `;
 }
 
-renderCharacter(hero);
-renderCharacter(monster);
+renderCharacter(characters.hero);
+renderCharacter(characters.monster);
