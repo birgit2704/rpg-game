e3325df490c9cf1ced3aaede5e75663030a806fd
Birgit Bartenschlager <birgit.bartenschlager@web.de>
2023-07-13 10:36:15 +0200

    restart from scratch for training

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-import characters from "./data.js";
-import Character from "./Character.js";
-
-const wizard = new Character(characters.hero);
-const orc = new Character(characters.monster);
-
-function render() {
-  document.getElementById("hero").innerHTML = wizard.renderCharacterHtml();
-  document.getElementById("monster").innerHTML = orc.renderCharacterHtml();
-}
-
-render();
+const characterData = {
+  hero: {
+    name: "Wizard",
+    avatar: "images/wizard.png",
+    health: 60,
+    diceCount: 3,
+    currentDiceScore: [],
+  },
+  orc: {
+    name: "Orc",
+    avatar: "images/orc.png",
+    health: 30,
+    diceCount: 1,
+    currentDiceScore: [],
+  },
+  demon: {
+    name: "Demon",
+    avatar: "images/demon.png",
+    health: 25,
+    diceCount: 2,
+    currentDiceScore: [],
+  },
+  goblin: {
+    name: "Goblin",
+    avatar: "images/goblin.png",
+    health: 20,
+    diceCount: 3,
+    currentDiceScore: [],
+  },
+};
